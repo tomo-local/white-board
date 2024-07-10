87023eb0c7af1eccb39bb7a523eb45bcdb61ec29
tomo-local <shinsi4545@gmail.com>
2024-07-10 23:26:53 +0900

    NodeのhookのカスタムHookの追加

--- src/hooks/useNodeControl.ts
+++ src/hooks/useNodeControl.ts
@@ -1,22 +1,33 @@
-import { useState, useEffect } from "react";
-import type { NodeProps } from "reactflow";
-import { useAtom } from "jotai";
+import { useState, useEffect, useCallback } from "react";
+import { useParams } from "next/navigation";
 
-import { nodeAtom, type NodeData } from "@/jotai/flow/node";
+import type { NodeProps } from "reactflow";
+import { useAtomCallback } from "jotai/utils";
+import type { NodeData } from "@/jotai/flow/node";
+import { nodesAtom } from "@/jotai/flow/page";
 
 export const useNodeControl = (initNode: NodeProps<NodeData>) => {
-  const [data, setData] = useState(initNode.data);
-  const [node, setNode] = useAtom(nodeAtom(initNode.id));
+  const { id }: { id: string } = useParams();
+  const [data, setData] = useState(initNode);
 
   useEffect(() => {
-    setData(data);
-  }, [data]);
+    setData(initNode);
+  }, [initNode]);
 
   return {
     data,
-    onChange: (newData: NodeData) => setData(newData),
-    onSave: () => {
-      setNode({ ...node, data });
-    },
+    onChange: (newData: NodeProps<NodeData>) => setData(newData),
+    onSave: useAtomCallback(
+      useCallback(
+        (get, set) => {
+          const nodes = get(nodesAtom(id));
+          const newNodes = nodes.map((n) =>
+            n.id === data.id ? { ...n, data: data.data } : n
+          );
+          set(nodesAtom(id), newNodes);
+        },
+        [data, id]
+      )
+    ),
   };
 };
