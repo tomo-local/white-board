158d182da7b20b8198e0f55be8907c9b705dafe0
tomo-local <shinsi4545@gmail.com>
2024-07-11 19:19:33 +0900

    💄 Nodeが選択の際のスタイル変更

--- src/components/custom/MarkdownNode.tsx
+++ src/components/custom/MarkdownNode.tsx
@@ -15,7 +15,11 @@ export default function NormalNode(props: NodeProps<NodeData>) {
 
   return (
     <>
-      <div className="px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-500 text-black">
+      <div
+        className={`px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-500 text-black ${
+          props.selected ? "border-4" : "border-2"
+        } `}
+      >
         <div className="flex flex-col w-40 h-20 max-h-20 max-w-40 justify-center">
           <div className="text-xs absolute top-2 left-3 flex justify-between text-stone-400">
             Type: Markdown
