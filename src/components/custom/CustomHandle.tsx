9662da5064c301c08521e74ec088e1bef9656735
tomo-local <shinsi4545@gmail.com>
2024-07-14 18:48:57 +0900

    🎨 二つのcommitの内容を適応

--- src/components/custom/CustomHandle.tsx
+++ src/components/custom/CustomHandle.tsx
@@ -1,12 +1,21 @@
+import React, { type HTMLAttributes } from "react";
 import { Handle, type Connection, type HandleProps } from "reactflow";
+import { clsx } from "clsx";
 
-type CustomHandleProps = Omit<HandleProps, "isValidConnection">;
+type CustomHandleProps = HandleProps &
+  Omit<HTMLAttributes<HTMLDivElement>, "id">;
 
 export default function CustomHandle(props: CustomHandleProps) {
-
   const isValidConnection = (connection: Connection) => {
     return connection.source !== connection.target;
   };
 
-  return <Handle {...props} isValidConnection={isValidConnection} />;
+  return (
+    <Handle
+      {...props}
+      className={clsx(["!bg-stone-800", props.className])}
+      isValidConnection={isValidConnection}
+      style={{ borderRadius: "50%", ...props.style }}
+    />
+  );
 }
--- src/components/custom/node/Markdown.tsx
+++ src/components/custom/node/Markdown.tsx
@@ -1,8 +1,8 @@
 "use client";
 import type { ChangeEvent } from "react";
-import { Position, type NodeProps } from "reactflow";
+import type { NodeProps } from "reactflow";
 
-import CustomHandle from "@/components/custom/CustomHandle";
+import CommonNode from "@/components/custom/node/CommonNode";
 import { useNodeControl } from "@/hooks/useNodeControl";
 
 type NodeData = {
@@ -17,19 +17,13 @@ export default function MarkdownNode(props: NodeProps<NodeData>) {
   };
 
   return (
-    <>
-      <div
-        className={`px-4 py-2 shadow-md rounded-md bg-white border-2 border-stone-500 text-black ${
-          props.selected ? "border-4" : "border-2"
-        } `}
-      >
-        <div className="flex flex-col w-40 h-20 max-h-20 max-w-40 justify-center">
-          <div className="text-xs absolute top-2 left-3 flex justify-between text-stone-400">
-            Type: Markdown
-          </div>
-          <div className="text-xs absolute top-2 right-3 flex justify-between text-stone-400">
-            ⚫️
-          </div>
+    <CommonNode
+      type="markdown"
+      dragging={props.dragging}
+      selected={props.selected}
+    >
+      <div className="text-black">
+        <div className="flex flex-col w-40 h-16 py-1 max-w-40 max-h-16">
           <input
             type="text"
             value={data.data.label}
@@ -44,11 +38,6 @@ export default function MarkdownNode(props: NodeProps<NodeData>) {
           />
         </div>
       </div>
-
-      <CustomHandle type="source" position={Position.Top} id="top" />
-      <CustomHandle type="source" position={Position.Right} id="right" />
-      <CustomHandle type="target" position={Position.Left} id="left" />
-      <CustomHandle type="target" position={Position.Bottom} id="bottom" />
-    </>
+    </CommonNode>
   );
 }
