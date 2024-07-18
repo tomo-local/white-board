312ff56dde3647aa21209d4047ee01957a89ef57
tomo-local <shinsi4545@gmail.com>
2024-07-19 02:14:38 +0900

    🎨 Node Listを追加

--- src/app/reactflow/[id]/page.tsx
+++ src/app/reactflow/[id]/page.tsx
@@ -5,12 +5,13 @@ import styles from "@/app/reactflow/page.module.css";
 import "@/app/globals.css";
 import Flow from "@/components/Flow";
 import SidePeek from "@/components/editor/SidePeek";
+import NodeListSideView from "@/components/NodeListSideView";
 
 export default function ReactFlowPage({ params }: { params: { id: string } }) {
   return (
     <main className={styles.main}>
-      <div className="absolute">{params.id}</div>
       <ReactFlowProvider>
+        <NodeListSideView />
         <Flow />
         <SidePeek />
       </ReactFlowProvider>
