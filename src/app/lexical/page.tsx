e82fe582249411bf4a3883e7571dedb4bc3e638a
tomo-local <shinsi4545@gmail.com>
2024-08-03 03:15:41 +0900

    🎨 widthのminを設定

--- src/app/lexical/page.tsx
+++ src/app/lexical/page.tsx
@@ -8,7 +8,7 @@ export default function LexicalPage() {
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-4xl font-bold">Lexical</h1>
         <p className="text-lg">This is the Lexical page.</p>
-        <div className="mt-5 w-96 p-2 rounded-md bg-stone-100 text-stone-950">
+        <div className="mt-5 min-w-[50%] p-2 rounded-md bg-stone-100 text-stone-950">
           <RichEditor />
         </div>
       </div>
