ffa385d56908a3c4c6f08b1c37ea8df1b999cb33
tomo-local <shinsi4545@gmail.com>
2024-08-03 00:52:58 +0900

    ✨ add RichEditor in lexical page

--- src/app/lexical/page.tsx
+++ src/app/lexical/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import RichEditor from "@/components/common/editor/RichEditor";
+
 export default function LexicalPage() {
   return (
     <main className="">
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-4xl font-bold">Lexical</h1>
         <p className="text-lg">This is the Lexical page.</p>
+        <div className="mt-5 w-96 p-2 rounded-md bg-stone-100 text-stone-950">
+          <RichEditor />
+        </div>
       </div>
     </main>
   );
