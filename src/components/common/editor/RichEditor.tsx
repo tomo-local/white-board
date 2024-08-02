8b5c35d5ff6898efb04b615efe5fd0c3d7ede1d5
tomo-local <shinsi4545@gmail.com>
2024-08-03 00:51:37 +0900

    ✨ Add RichEditor component

---
+++ src/components/common/editor/RichEditor.tsx
@@ -0,0 +1,34 @@
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+
+const theme = {};
+
+function onError(error: Error) {
+  console.error(error);
+}
+
+export default function RichEditor() {
+  const initialConfig = {
+    namespace: "MyEditor",
+    theme,
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <RichTextPlugin
+        contentEditable={
+          <ContentEditable className="focus:outline-none focus:shadow-outline" />
+        }
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+      <AutoFocusPlugin />
+    </LexicalComposer>
+  );
+}
