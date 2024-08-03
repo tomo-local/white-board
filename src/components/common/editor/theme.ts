ed51e57c63cd28c5ac281ed7484dbd25f351492c
tomo-local <shinsi4545@gmail.com>
2024-08-03 16:34:34 +0900

    ✨ feat: Add ToolbarPlugin component for rich text editing

---
+++ src/components/common/editor/theme.ts
@@ -0,0 +1,33 @@
+export default {
+  code: 'editor-code',
+  heading: {
+    h1: 'text-4xl',
+    h2: 'text-3xl',
+    h3: 'text-2xl',
+    h4: 'text-xl',
+    h5: 'text-lg',
+  },
+  list: {
+    listitem: 'editor-listitem',
+    nested: {
+      listitem: 'editor-nested-listitem',
+    },
+    ol: 'editor-list-ol',
+    ul: 'editor-list-ul',
+  },
+  ltr: 'ltr',
+  paragraph: 'editor-paragraph',
+  placeholder: 'editor-placeholder',
+  quote: 'editor-quote',
+  rtl: 'rtl',
+  text: {
+    bold: 'font-bold',
+    code: 'bg-gray-200 py-1 px-2',
+    hashtag: 'editor-text-hashtag',
+    italic: 'italic',
+    overflowed: 'editor-text-overflowed',
+    strikethrough: 'line-through',
+    underline: 'underline',
+    underlineStrikethrough: 'underline line-through',
+  },
+};
