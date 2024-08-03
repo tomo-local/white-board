338d1085b8fcdf162f769c7df41f9a1923b77f13
tomo-local <shinsi4545@gmail.com>
2024-08-03 16:34:51 +0900

    ✨ feat: Add underline and line-through text decoration style

--- src/components/common/editor/RichEditor.tsx
+++ src/components/common/editor/RichEditor.tsx
@@ -6,8 +6,9 @@ import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
 
 import TreeViewPlugin from "@/components/common/editor/plugins/TreeViewPlugin";
+import ToolbarPlugin from "@/components/common/editor/plugins/ToolBarPlugin";
 
-const theme = {};
+import theme from "@/components/common/editor/theme";
 
 function onError(error: Error) {
   console.error(error);
@@ -22,16 +23,21 @@ export default function RichEditor() {
 
   return (
     <LexicalComposer initialConfig={initialConfig}>
-      <RichTextPlugin
-        contentEditable={
-          <ContentEditable className="focus:outline-none focus:shadow-outline" />
-        }
-        placeholder={<div>Enter some text...</div>}
-        ErrorBoundary={LexicalErrorBoundary}
-      />
-      <HistoryPlugin />
-      <AutoFocusPlugin />
-      <TreeViewPlugin />
+      <div className="h-[70vh]">
+        <ToolbarPlugin />
+        <div className="max-h-[45%] min-h-[45%] overflow-scroll pt-1">
+          <RichTextPlugin
+            contentEditable={
+              <ContentEditable className="focus:outline-none focus:shadow-outline" />
+            }
+            placeholder={<div>Enter some text...</div>}
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+        </div>
+        <HistoryPlugin />
+        <AutoFocusPlugin />
+        <TreeViewPlugin />
+      </div>
     </LexicalComposer>
   );
 }
