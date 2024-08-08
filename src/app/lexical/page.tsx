b0b1424b1e2133884dce5809e4364f1171cc0c79
tomo-local <shinsi4545@gmail.com>
2024-08-08 23:59:20 +0900

    🎨 main をpageから layoutへ移動

--- src/app/lexical/page.tsx
+++ src/app/lexical/page.tsx
@@ -1,17 +1,14 @@
 "use client";
-
 import RichEditor from "@/components/common/editor/RichEditor";
 
 export default function LexicalPage() {
   return (
-    <main className="">
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold">Lexical</h1>
-        <p className="text-lg">This is the Lexical page.</p>
-        <div className="mt-5 min-w-[50%] p-2 rounded-md bg-stone-100 text-stone-950">
-          <RichEditor isDev className="h-[70vh]" />
-        </div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Lexical</h1>
+      <div className="text-lg">This is the Lexical page.</div>
+      <div className="mt-5 min-w-[50%] p-2 rounded-md bg-stone-100 text-stone-950">
+        <RichEditor isDev className="h-[70vh]" />
       </div>
-    </main>
+    </div>
   );
 }
